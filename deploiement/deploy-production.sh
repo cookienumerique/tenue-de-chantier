cc74dee1cc796549816b2255a384422feda14f8d
Cavelier <cookienumerique@gmail.com>
2024-02-14 09:25:59 +0100

    Build: Change script deploy

--- deploiement/deploy-production.sh
+++ deploiement/deploy-production.sh
@@ -3,7 +3,7 @@ rm -rf .next;
 npm install --max-old-space-size=4096;
 # Mettre sur stop les docker de l'application
 docker ps -a --filter "name=_tenue_de_chantiers" -q | xargs docker stop
-node --max-old-space-size=8000 $(which npm) build
+node --max-old-space-size=8000 $(which npm) run build
 #pm2 start npm --name "tenue-de-chantiers" -- start;
 pm2 restart tenue-de-chantiers
 docker ps -a --filter "name=_tenue_de_chantiers" -q | xargs docker start
