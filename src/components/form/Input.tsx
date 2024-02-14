d708f789a0160e1f3abd3600f2aad27c00ad2f08
Cavelier <cookienumerique@gmail.com>
2024-02-14 09:26:10 +0100

    Feat : create Input component

---
+++ src/components/form/Input.tsx
@@ -0,0 +1,66 @@
+import { InputProps as InputChakraProps } from '@chakra-ui/input';
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input as InputChakra,
+} from '@chakra-ui/react';
+import { useField } from '@formiz/core';
+import { ChangeEvent, ReactElement, useRef } from 'react';
+
+type InputProps = InputChakraProps & {
+  name: string;
+  required?: boolean;
+  label: string;
+  helperMessage?: string;
+};
+export default function Input(
+  props: InputProps
+): ReactElement {
+  const { required, label, helperMessage } = props;
+  const ref = useRef<HTMLInputElement>();
+  const {
+    value,
+    setValue,
+    errorMessage,
+    isValid,
+    isSubmitted,
+  } = useField(props, {
+    formatValue: (value: string | null) => value,
+  });
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    setValue(event?.target?.value);
+  };
+
+  return (
+    <FormControl
+      isInvalid={!isValid}
+      isRequired={required}
+    >
+      <FormLabel aria-required={required}>
+        {label}
+      </FormLabel>
+      <InputChakra
+        ref={ref}
+        onChange={handleChange}
+        {...props}
+      />
+
+      {helperMessage ? (
+        <FormHelperText>{helperMessage}</FormHelperText>
+      ) : null}
+      {!isValid ? (
+        <FormErrorMessage>
+          {required && !value && isSubmitted
+            ? 'Ce champ est obligatoire'
+            : ''}
+          {errorMessage}
+        </FormErrorMessage>
+      ) : null}
+    </FormControl>
+  );
+}
