70bd71f3112b9612ff5727ed36218ba376f24ab0
Cavelier <cookienumerique@gmail.com>
2024-03-12 10:37:55 +0100

    Feat: Redirect home on title

--- src/components/layout/header/HeaderTitle.tsx
+++ src/components/layout/header/HeaderTitle.tsx
@@ -1,4 +1,5 @@
 import { ResponsiveValue, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import { ReactElement } from 'react';
 
 type HeaderTitleProps = {
@@ -10,12 +11,15 @@ export default function HeaderTitle(
   props: HeaderTitleProps
 ): ReactElement {
   const { fontSize } = props;
+  const { push } = useRouter();
   return (
     <Text
       fontSize={fontSize}
       color="gray.800"
       fontWeight="bold"
       textAlign="center"
+      onClick={() => push('/')}
+      cursor="pointer"
     >
       {process?.env?.NEXT_PUBLIC_APP_NAME?.toUpperCase() ??
         'My app'}
