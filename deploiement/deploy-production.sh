03f3905e7b28fbbb4397e021221d8d914e78488d
Cavelier <cookienumerique@gmail.com>
2024-04-02 11:39:22 +0200

    Build: change deploy script

--- deploiement/deploy-production.sh
+++ deploiement/deploy-production.sh
@@ -1,9 +1,9 @@
+#!/bin/zsh
+
+git pull origin main;
 rm -rf node_modules;
-rm -rf .next;
-npm install --max-old-space-size=4096;
-# Mettre sur stop les docker de l'application
-docker ps -a --filter "name=_tenue_de_chantiers" -q | xargs docker stop
-node --max-old-space-size=8000 $(which npm) run build
-#pm2 start npm --name "tenue-de-chantiers" -- start;
-pm2 restart tenue-de-chantiers
-docker ps -a --filter "name=_tenue_de_chantiers" -q | xargs docker start
+rm -rf .next
+npm install;
+npm run build;
+#pm2 start npm --name "tenue-de-chantiers" -- run start -- --port 3001
+pm2 restart tenue-de-chantiers;
